05f805c7cca98157a89d978106dc840d7aaa8916
SoraKatadzuma <dungeonsandanime@gmail.com>
2020-11-02 09:58:39 -0600

    Fixed TS2740

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import ReactDOM from "react-dom";
 import { Frame } from "./frame"
 import { Panel } from "./panels/panel"
 
-let rootFrame: Frame = <Frame><Panel></Panel></Frame>
+let rootFrame: ReactElement = <Frame><Panel></Panel></Frame>
 
 ReactDOM.render(rootFrame, document.getElementById("root"));
