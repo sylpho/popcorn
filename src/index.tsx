94d357309b2b9134e3600d398f602d5adb5911c4
SoraKatadzuma <dungeonsandanime@gmail.com>
2020-11-02 00:40:01 -0600

    webpack incoming

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,22 @@
-const element = <div class="frame" style="grid: 100% / repeat(2, 50%)">
-	<div style="display: block; background-color: #ff0000"></div>
-	<div style="display: block; background-color: #ff0000"></div>
-</div>;
+import React 		from "react";
+import ReactDOM from "react-dom";
+
+class Frame extends React.Component {
+	render() {
+		return (
+			<div class="frame" style="grid: 100% / repeat(2, 50%)">
+				<Panel/>
+			</div>
+		);
+	}
+}
+
+class Panel extends React.Component {
+	render() {
+		return (
+			<div style="display: block; background-color: #ff0000"></div>
+		);
+	}
+}
+
+ReactDOM.render(new Frame(), document.getElementById("root"));
